ea5d0bcac9b216f95429567634f5a30809226d9d
Kevin Neely <ktneely@astroturfgarden.com>
2013-09-24 18:11:28 -0700

    bare bones and will image filevault drives only

--- create_image.sh
+++ create_image.sh
@@ -1,33 +1,43 @@
 #! /bin/bash
 
 # A script to create an image of a drive, stolen from the review_drive script
-# 
+# Version 0.2-beta
 # TODO
 # - create handling for plain, bitlocker, and filevault drives
+# 0.2
+# ??
 # 0.1
 # Simply a cut and paste from the previous do-everthing script. Probably doesn't work
 
 
 # Functions
 collect_info () {
-#    echo "What is the original drive device (e.g. /dev/sdc1)"
-#    read DEVICE
-    echo "What is the victim's name?"
-    read VICTIM
-    echo "What type of system did the drive come from? (e.g. t400)"
-    read SYSTEM_TYPE
-    SYSTEM=$VICTIM-$SYSTEM_TYPE
+    echo "What is the original drive device (e.g. /dev/sdc1)"
+    read DEVICE
+    DATE=`date +%Y%m%d-%T`
+    echo "What is the name of the custodian? (def: corpuser)"
+    read custodian_name
+    CUSTODIAN=${custodian_name:=corpuser}
+    echo "What is the system model? (def: laptop)"
+    read system_model
+    SYSTEM_TYPE=${system_model:=laptop}
+    SYSTEM=$CUSTODIAN-$SYSTEM_TYPE
+    echo "What path should be used for temp space? (def: /Data/tmp)"
+    read temp_space
+    TEMP=${temp_space:=/Data/tmp/$SYSTEM}
     IMAGE=$SYSTEM.dd
-    TEMP=/Data/tmp/$SYSTEM
-    echo "Enter the case identifier def: [2011-04-07a-crypto]"
+    echo "Enter the case identifier. (def: [2011-04-07a-crypto]"
     read input_case
-    CASE_NAME=${input_case:=2011-04-07a-crypto}
+    CASE_NAME=${input_case:=}
     EXAMINE_DIR=$TEMP/examine
     mkdir -p $EXAMINE_DIR
-    echo "Enter the image repository def: [/mnt/stor/Images]"
+    echo "Enter the image repository. def: [/mnt/stor/Images]"
     read input_image
-    IMAGE_REPOSITORY=${input_image:=/mnt/stor/Images/}
+    IMAGE_REPOSITORY=${input_image:=/Data/tmp}
     IMAGE_DIR=$IMAGE_REPOSITORY/$CASE_NAME
+    echo "What is the decryption key?"
+    read DECRYPTION_KEY
+    mkdir -p $TEMP
     mkdir -p $IMAGE_DIR
     }
 
@@ -36,7 +46,6 @@ check_program () {  # checks is a program exists on the system
     echo "check for existence of $1"
     type "$1" &> /dev/null
 }
-
 
 # create destination 
 create_examine () {
@@ -47,3 +56,30 @@ create_examine () {
 	echo Examination dir $EXAMINE_DIR has been created;
     fi
 }
+
+mount_image () {
+# look for empty 'examine' dir under /mnt
+echo "Attempting to mount device $1" >> $TEMP/Manifest.lst
+blkid $IMAGE >> $TEMP/Manifest.lst
+for (( i = 1 ; i <= 8; i++ ))
+do
+    EXAMINE_DIR="/mnt/examine$i";
+    if find $EXAMINE_DIR -maxdepth 0 -empty | read v;  # find an unused mount point
+    then 
+	echo "empty examination directory found at /mnt/examine$i";
+	echo "$IMAGE"
+	bdemount -r $DECRYPTION_KEY $DEVICE $EXAMINE_DIR
+	echo "Mounted $IMAGE at /mnt/examine$i" >> $TEMP/Manifest.lst
+	i=200;   # set i arbitrarily high to break the loop
+    else 
+	echo "Mounting $IMAGE at /mnt/examine$i failed" 
+    fi
+done
+    }
+
+
+# begin main program
+collect_info
+mount_image
+dc3dd if=$EXAMINE_DIR/bde1 of=$IMAGE_DIR/$IMAGE
+umount $EXAMINE_DIR
