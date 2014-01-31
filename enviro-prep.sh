ff66c6c87192866b48eedaa7b2306d10c4b007cd
Kevin Neely <ktneely@astroturfgarden.com>
2014-01-30 22:46:21 -0800

    adding enviro-prep

---
+++ enviro-prep.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# v 0.1
+#
+# This is meant to prep your environment for the other scripts in this
+# repository
+
+check_program () {  # checks is a program exists on the system
+    echo "check for existence of $1"
+    type "$1" &> /dev/null
+}
+
+# create the necessary directories under /Data
+
+echo "This script assumes you have write access to / or /Data"
+mkdir -p /Data/Forensics
+mkdir -p /Data/Hashes/NSRL
+mkdir /Data/Hashes/Malware
+mkdir /Data/Wordlists
+mkdir /Data/Malware
+
+# install some dependencies
+if check_program apt-get; then
+    sudo apt-get install dc3dd clamav md5deep
+elif check_program yum; then
+    yum install dc3dd md5deep
+fi
