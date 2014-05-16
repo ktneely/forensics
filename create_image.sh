411158163131cca4abe9ee9e23a9de7286c457ab
Kevin <ktneely@astroturfgarden.com>
2014-05-16 15:20:08 -0700

    fixed bugs in device logging and info collection

--- create_image.sh
+++ create_image.sh
@@ -12,6 +12,7 @@
 #     archive location
 # - Archives default to 7zip but fall-back to gzip
 # - Inventory file created & updated on archive location
+# - Physical drive and logical partition info added to log file
 # - Considerably lowered 7z compression level
 # - modified many defaults for more streamlined workflow
 # 0.2
@@ -27,6 +28,7 @@
 collect_info () {
     echo "What is the original drive device (e.g. /dev/sdc1)"
     read DEVICE
+    PARTITION=`echo $DEVICE | awk -F'/' '{print $3}'`
     DATE=`date +%Y%m%d-%T`
     echo "What is the name of the custodian? (def: corpuser)"
     read custodian_name
@@ -44,11 +46,10 @@ collect_info () {
 #    CASE_NAME=${input_case:=`date +%Y-%m-%da`}
     CASE_NAME=${input_case:=WSGR-SEC-01}
     EXAMINE_DIR=$TEMP/examine
-    mkdir -p $EXAMINE_DIR
-    echo "Enter the image repository. def: [/Data/Forensics/$CASE_NAME/images]"
+    echo "Enter the image repository. (def: [/Data/Forensics/$CASE_NAME/images])"
     read input_image
     IMAGE_DIR=${input_image:=/Data/Forensics/$CASE_NAME/images}
-    echo "Enter the location for archives. def [/Data/archive/$CASE_NAME]"
+    echo "Enter the location for archives. (def: [/Data/archive/$CASE_NAME])"
     read input_archive
     ARCHIVE_DIR=${input_archive:=/Data/archive/$CASE_NAME}
     echo "What is the decryption key? (def: 'none' for no encryption)"
@@ -74,7 +75,7 @@ create_examine () {
     fi
 }
 
-mount_image () {
+create_image () {
 # look for empty 'examine' dir under /mnt
 echo "Attempting to mount device $1" >> $TEMP/Manifest.lst
 blkid $IMAGE >> $TEMP/Manifest.lst
@@ -88,14 +89,14 @@ do
 	if [[ "$DECRYPTION_KEY" == "none" ]];
 	    then
 	    if check_program dc3dd; then
-		dc3dd if=$DEVICE of=$IMAGE_DIR/$IMAGE hash=md5 hash=sha1 hlog=/$IMAGE_DIR/$IMAGE-hash.log 
+		dc3dd if=$DEVICE of=$IMAGE_DIR/$IMAGE hash=md5 hash=sha1 hlog=/$IMAGE_DIR/$IMAGE-info.log 
 		else dd if=$DEVICE of=$IMAGE_DIR/$IMAGE
 	    fi
 	    else
 	    bdemount -r $DECRYPTION_KEY $DEVICE $EXAMINE_DIR
 	    echo "Mounted $IMAGE at /mnt/examine$i" >> $TEMP/Manifest.lst
 	    if check_program dc3dd; then
-		dc3dd if=$EXAMINE_DIR/bde1 of=$IMAGE_DIR/$IMAGE hash=md5 hash=sha1 hlog=/$IMAGE_DIR/$IMAGE-hash.log 
+		dc3dd if=$EXAMINE_DIR/bde1 of=$IMAGE_DIR/$IMAGE hash=md5 hash=sha1 hlog=/$IMAGE_DIR/$IMAGE-info.log 
 		else dd if=$EXAMINE_DIR/bde1 of=$IMAGE_DIR/$IMAGE
 	    fi
 	fi
@@ -110,27 +111,35 @@ archive () {
 # creates a compressed copy of the image for archival purposes
 # uses defined $TEMP for temp space then copies to $ARCHIVE_DIR/$SYSTEM.7z
 if check_program 7z; then
-    7z a -w$TEMP -mx=4 -m0=lzma2 $ARCHIVE_DIR/$SYSTEM.7z $IMAGE_DIR/$IMAGE $IMAGE_DIR/$IMAGE-hash.log
+    7z a -w$TEMP -mx=4 -m0=lzma2 $ARCHIVE_DIR/$SYSTEM.7z $IMAGE_DIR/$IMAGE $IMAGE_DIR/$IMAGE-info.log
     else
     gzip -c $IMAGE_DIR/$IMAGE > $ARCHIVE_DIR/$SYSTEM.gz
 fi
 }
 
+drive_info () {   # adds physical drive information to the log
+echo -e "Logical drive information\n" >> $IMAGE_DIR/$IMAGE-info.log
+cat /proc/partitions |egrep "major | $PARTITION" >> $IMAGE_DIR/$IMAGE-info.log
+echo -e "\n Physical drive information\n"  >>$IMAGE_DIR/$IMAGE-info.log
+hdparm -I $DEVICE >> $IMAGE_DIR/$IMAGE-info.log
+}
+
+
 inventory () {   # adds a line to the inventory file on the archive destination
-if [ -f "$ARCHIVE_DIR/inventory.txt" ]
+if [ -f "$ARCHIVE_DIR/../inventory.txt" ]
     then
-   echo -e "`date +%Y/%m/%d`, $CASE_NAME, $CUSTODIAN, $SYSTEM_TYPE" >> $ARCHIVE_DIR/inventory.txt
+    echo -e "`date +%Y/%m/%d`,$CASE_NAME,$CUSTODIAN,$SYSTEM_TYPE,$CASE_NAME/$SYSTEM.7z" >> $ARCHIVE_DIR/../inventory.txt
 else
-   echo -e "Date, Case Number, Custodian, System, File Location" > $ARCHIVE_DIR/inventory.txt
-   echo -e "`date +%Y/%m/%d`, $CASE_NAME, $CUSTODIAN, $SYSTEM_TYPE, $ARCHIVE_DIR/$SYSTEM.7z" >> $ARCHIVE_DIR/inventory.txt
+    echo -e "Date,Case Number,Custodian,System,File Location" > $ARCHIVE_DIR/../inventory.txt
+    echo -e "`date +%Y/%m/%d`,$CASE_NAME,$CUSTODIAN,$SYSTEM_TYPE,$CASE_NAME/$SYSTEM.7z" >> $ARCHIVE_DIR/../inventory.txt
 fi
 }
 
 
 # begin main program
 collect_info
-mount_image
+create_image
+drive_info
 umount /mnt/examine$i
 archive
 inventory
-
