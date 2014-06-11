31f85784683a2dc150d0f4b8861276ea2a1c3fb5
Kevin Neely <ktneely@astroturfgarden.com>
2014-06-11 15:24:02 -0700

    added logic to check that destination file is identical to the source file

--- copy_evidence.sh
+++ copy_evidence.sh
@@ -9,6 +9,10 @@
 #
 # NOTE:  Destination should be the root of your archive mount
 #
+# TODO: 
+#   - more elegant failure of a match
+#   - deletion of source material (make it an option?)
+#   - not such an ugly multi-nested if statement?
 
 
 # Functions
@@ -22,21 +26,28 @@ collect_info () {
     echo "Enter the location for archives. (def: [/Data/archive/$CASE_NAME/$EVIDENCE_TYPE])"
     read input_archive
     ARCHIVE_DIR=${input_archive:=/Data/archive/$CASE_NAME/$EVIDENCE_TYPE}
+    mkdir -p $ARCHIVE_DIR
     }
 
 copy_and_log () {
     for f in $1/* ; do
     if [ -f "$f" ]; then  # progress through all files in the dir
-	F_MD5SUM=`md5deep -b $f |awk '{print $1}'`    # calc md5
+	F_MD5SUM=`md5deep -b $f |awk '{print $1}'`    # calc md5 of original
 	cp $f $ARCHIVE_DIR          # copy each file
-	if [ -f "$ARCHIVE_DIR/../../inventory.txt" ]; then
-	    echo -e "`date +%Y/%m/%d`,$CASE_NAME,"",$EVIDENCE_TYPE,$CASE_NAME/$EVIDENCE_TYPE/${f##*/},$F_MD5SUM" >> $ARCHIVE_DIR/../../inventory.txt
-	else
-	    echo -e "Date,Case Number,Custodian,System,File Location,Checksum" > $ARCHIVE_DIR/../../inventory.txt
-	    echo -e "`date +%Y/%m/%d`,$CASE_NAME,"",$EVIDENCE_TYPE,$CASE_NAME/$EVIDENCE_TYPE/${f##*/},$F_MD5SUM" >> $ARCHIVE_DIR/../../inventory.txt
-	fi
+	C_MD5SUM=`md5deep -b $ARCHIVE_DIR/${f##*/} |awk '{print $1}'` # calc md5 of copy
+	if [ "$F_MD5SUM" == "$C_MD5SUM" ]; then
+	    if [ -f "$ARCHIVE_DIR/../../inventory.txt" ]; then
+		echo -e "`date +%Y/%m/%d`,$CASE_NAME,"",$EVIDENCE_TYPE,$CASE_NAME/$EVIDENCE_TYPE/${f##*/},$F_MD5SUM" >> $ARCHIVE_DIR/../../inventory.txt
+	    else
+		echo -e "Date,Case Number,Custodian,System,File Location,Checksum" > $ARCHIVE_DIR/../../inventory.txt
+		echo -e "`date +%Y/%m/%d`,$CASE_NAME,"",$EVIDENCE_TYPE,$CASE_NAME/$EVIDENCE_TYPE/${f##*/},$F_MD5SUM" >> $ARCHIVE_DIR/../../inventory.txt
+	    fi
+	fi	
+    else
+	echo "Failed on $f"
+	exit 1
     fi
-done
+    done
 }
 
 ###
